10c229d479a30316f902ae885503db8fc215839f
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com.>
2024-12-18 13:33:00 -0300

    feat(home/index): ✨ slugs logic
    make slugify_title fn for dynamically making dummy slugs in posts

--- blog/data/posts.py
+++ blog/data/posts.py
@@ -1,4 +1,14 @@
-posts = [
+from django.utils.text import slugify
+
+def slugify_title(title):
+  title_words = title.strip().split(" ")
+  if len(title_words) > 5:
+    title_words = title_words[:6]    
+  formatted_title_words = [word.lower() for word in title_words]
+  slug = slugify("-".join(formatted_title_words))
+  return slug
+
+posts_no_url = [
   {
     "id":0,
     "title":"Cillum irure mollit reprehenderit aliqua voluptate veniam eu",
@@ -29,3 +39,10 @@ posts = [
     "content":"Sit aute cillum reprehenderit commodo irure duis minim mollit deserunt. Qui ad Lorem est id exercitation incididunt ea. Elit deserunt dolor Lorem nisi do veniam non. Exercitation deserunt reprehenderit esse labore anim sit incididunt enim in adipisicing.\nIrure eiusmod cillum velit Lorem do laboris mollit et. Tempor id et voluptate laborum laboris amet consectetur. Lorem esse non aliquip elit dolor nisi anim laboris cupidatat nulla ea et dolore aliqua. Pariatur aute adipisicing aliquip amet tempor irure cillum amet nisi sint exercitation minim esse laborum. Mollit esse eu ipsum minim. Irure ut mollit nostrud ex excepteur sint nostrud officia ex minim consectetur. Deserunt ullamco laboris reprehenderit ad."
   },
 ]
+
+urls = [slugify_title(post["title"]) for post in posts_no_url]
+
+posts = [{
+  **post,
+  "slug": slugify_title(post["title"]) 
+} for post in posts_no_url]
--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 urlpatterns = [
     path("", views.home, name="home"),
     path("posts/", views.index, name="index"),
-    path("posts/<int:post>/", views.redirect_details, name="numeric-details"),
-    path("posts/<str:post>/", views.details, name="post-details"),
+    path("posts/<int:num>/", views.redirect_details, name="numeric-details"),
+    path("posts/<str:slug>/", views.details, name="post-details"),
 ]
